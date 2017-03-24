f3fa9665483eada4391afe70e092f371d5199e73
syedismailmca <syedismailabc@gmail.com>
2017-03-24 09:44:34 +0530

    holo

---
+++ git1/src/main/java/in/co/git1/App.java
@@ -0,0 +1,13 @@
+package in.co.git1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
